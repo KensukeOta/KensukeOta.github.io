d847c01c7f4a4282b9d00c4663ff6063531a0047
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2026-05-27 20:49:53 +0900

    Svelteをインストール

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [svelte()]
 });
