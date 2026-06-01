a9d5ff16b169d557204920876133e0da17171925
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2026-06-01 17:12:01 +0900

    デプロイ先をGitHub Pagesに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 import svelte from "@astrojs/svelte";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+  site: "https://kensukeota.github.io",
+
   integrations: [svelte()],
 
   vite: {
